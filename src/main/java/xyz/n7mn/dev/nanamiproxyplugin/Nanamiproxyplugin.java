eb9c10e4af83ea43a11eded70cf0da317d36266e
tina-chan <kassi_0123@kassi-hp.info>
2022-05-21 13:15:17 +0900

    進捗 (未デバッグ)

--- src/main/java/xyz/n7mn/dev/nanamiproxyplugin/Nanamiproxyplugin.java
+++ src/main/java/xyz/n7mn/dev/nanamiproxyplugin/Nanamiproxyplugin.java
@@ -21,6 +21,8 @@ import java.io.*;
 import java.nio.file.Files;
 import java.util.Base64;
 import java.util.Optional;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @Plugin(
         id = "nanamiproxyplugin",
@@ -46,22 +48,32 @@ public class Nanamiproxyplugin {
         plugin = proxyServer.getPluginManager().getPlugin("nanamiproxyplugin");
 
         File file1 = new File("./plugins/" + plugin.get().getDescription().getName().get());
-        File file2 = new File("./plugins/" + plugin.get().getDescription().getName().get() + "/config.yml");
-        File file3 = new File("./plugins/" + plugin.get().getDescription().getName().get() + "/server.yml");
+        File file2 = new File("./plugins/" + plugin.get().getDescription().getName().get() + "/server-7mi.xyz.yml");
+        File file3 = new File("./plugins/" + plugin.get().getDescription().getName().get() + "/config.yml");
         if (!file1.exists()){
             file1.mkdir();
         }
 
-        if (!file3.exists()){
+        if (!file2.exists()){
             YamlMappingBuilder builder = Yaml.createYamlMappingBuilder();
             YamlMapping mapping = builder.add(
-                    "7mi.xyz", "[1.8-1.18.2] ななみ鯖"
+                    "MinProtocolVer", "47"
+            ).add(
+                    "MaxProtocolVer", "758"
+            ).add(
+                    "VersionText","NanamiSystem 1.8-1.18.2"
+            ).add(
+                    "ServerID","0"
+            ).add(
+                    "ServerName","Sample"
+            ).add(
+                    "ServerText","[1.8-1.18.2] ななみ鯖"
             ).build();
 
             String yml = mapping.toString();
 
             try {
-                PrintWriter writer = new PrintWriter(file3);
+                PrintWriter writer = new PrintWriter(file2);
                 writer.print(yml);
                 writer.close();
             } catch (FileNotFoundException e) {
@@ -70,46 +82,78 @@ public class Nanamiproxyplugin {
 
         }
 
+
+        Timer timer = new Timer();
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                new Thread(()->{
+                    int size = proxyServer.getAllPlayers().size();
+
+                }
+                ).start();
+            }
+        }, 0L, 1000L);
+
     }
 
     @Subscribe
     public void ProxyPingEvent(ProxyPingEvent e){
         String hostName = e.getConnection().getVirtualHost().get().getHostName();
+        ServerPing ping = e.getPing();
+        ServerPing.Builder builder = ping.asBuilder();
+        Component text = ping.getDescriptionComponent();
 
         try {
-            YamlMapping mapping = Yaml.createYamlInput(new File("./plugins/" + plugin.get().getDescription().getName().get() + "/server.yml")).readYamlMapping();
-            // System.out.println("---- debug ----\n"+mapping.toString()+"\n---- debug ----");
 
-            String desc = mapping.string(hostName);
+            File file1 = new File("./plugins/" + plugin.get().getDescription().getName().get() + "/server-" + hostName + ".yml");
 
-            ServerPing ping = e.getPing();
-            ServerPing.Builder builder = ping.asBuilder();
+            if (file1.exists()){
+                YamlMapping mapping = Yaml.createYamlInput(file1).readYamlMapping();
+                // System.out.println("---- debug ----\n"+mapping.toString()+"\n---- debug ----");
 
-            if (desc == null){
-                return;
+                String desc = mapping.string("ServerText");
+                String verText = mapping.string("VersionText");
+
+                if (desc != null){
+                    text = Component.text(desc);
+                }
+
+                if (verText != null){
+
+                    int minProtocolVer = Integer.parseInt(mapping.string("MinProtocolVer"));
+                    int maxProtocolVer = Integer.parseInt(mapping.string("MaxProtocolVer"));
+
+                    if (e.getConnection().getProtocolVersion().getProtocol() >= minProtocolVer && e.getConnection().getProtocolVersion().getProtocol() <= maxProtocolVer){
+                        builder.version(new ServerPing.Version(e.getConnection().getProtocolVersion().getProtocol(), verText));
+                    } else if (e.getConnection().getProtocolVersion().getProtocol() >= maxProtocolVer){
+                        builder.version(new ServerPing.Version(maxProtocolVer, verText));
+                    } else if (e.getConnection().getProtocolVersion().getProtocol() <= minProtocolVer){
+                        builder.version(new ServerPing.Version(minProtocolVer, verText));
+                    } else {
+                        builder.version(new ServerPing.Version(maxProtocolVer, verText));
+                    }
+                    
+                }
             }
 
-            Component text = Component.text(desc);
-
-            if (desc.length() == 0){
-                text = ping.getDescriptionComponent();
-            }
 
             //String url = ping.getFavicon().get().getBase64Url();
             //System.out.println("icon : "+url);
 
-            File file = new File("./plugins/" + plugin.get().getDescription().getName().get() + "/" + hostName + ".png");
+            File file2 = new File("./plugins/" + plugin.get().getDescription().getName().get() + "/" + hostName + ".png");
             //System.out.println(file.toPath());
 
-            if (file.exists()){
+            if (file2.exists()){
                 //System.out.println("!!");
-                String contentType = Files.probeContentType(file.toPath());
+                String contentType = Files.probeContentType(file2.toPath());
 
                 StringBuilder sb = new StringBuilder();
                 sb.append("data:");
                 sb.append(contentType);
                 sb.append(";base64,");
-                sb.append(Base64.getEncoder().encodeToString(Files.readAllBytes(file.toPath())));
+                sb.append(Base64.getEncoder().encodeToString(Files.readAllBytes(file2.toPath())));
 
                 Favicon favicon = new Favicon(sb.toString());
                 //System.out.println(sb.toString());
